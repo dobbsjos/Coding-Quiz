71720c2662a498b282ea5d63c54c16d87b7f510a
Joshua Dobbs <dobbsjos@gmail.com>
2019-12-09 20:01:13 -0500

    declared variables

--- codingQuiz.js
+++ codingQuiz.js
@@ -1,1 +1,11 @@
-document.getElementById('answer-1').innerHTML
+var quest = document.getElementById('question').textContent = "";
+
+var ans1 = document.getElementById('answer-1').textContent = "";
+
+var ans2 = document.getElementById('answer-2').textContent = "";
+
+var ans3 = document.getElementById('answer-3').textContent = "";
+
+var ans4 = document.getElementById('answer-4').textContent = "";
+
+
