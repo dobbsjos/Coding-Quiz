e3d4cefc0ceafe963b12050238b993056db97fa6
Joshua Dobbs <dobbsjos@gmail.com>
2019-12-10 09:22:36 -0500

    what i finished

--- codingQuiz.js
+++ codingQuiz.js
@@ -1,3 +1,4 @@
+// Declare variables
 var quest = document.getElementById('question').textContent = "";
 
 var ans1 = document.getElementById('answer-1').textContent = "";
@@ -8,4 +9,14 @@ var ans3 = document.getElementById('answer-3').textContent = "";
 
 var ans4 = document.getElementById('answer-4').textContent = "";
 
+// build array's to hold the questions and anwswers
+var questions = {
+    question1: rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3
+}
 
+// Create a function to pass the Questions and answers into the page
+function renderQuestions() {
+    
+}
+
+renderQuestions();
