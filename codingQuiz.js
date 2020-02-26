8320db5a24b746b2ff388ed769a535ad231d968a
Joshua Dobbs <dobbsjos@gmail.com>
2020-02-25 21:48:00 -0500

    added jquery

--- codingQuiz.js
+++ codingQuiz.js
@@ -9,14 +9,33 @@ var ans3 = document.getElementById('answer-3').textContent = "";
 
 var ans4 = document.getElementById('answer-4').textContent = "";
 
-// build array's to hold the questions and anwswers
-var questions = {
-    question1: rightAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3
-}
-
 // Create a function to pass the Questions and answers into the page
 function renderQuestions() {
+    var questions = [
+        {title: "What is your favorate color?",
+        choices:["red", "yellow", "blue", "blue, no green"],
+        answer: "blue, no green"
+        },
+        {title: "what is the air speed of a swallow?",
+        choices:["90 meters per hour", "African or European?", "numbers", "strings"],
+        answer:"parameters"
+        }
+    ];
     
+    $(".start").on("click", function(){
+        startGame();
+        });
+    
+    function startGame(){
+    
+        //Make game intro screen not visible and questions visible
+        $("#intro").attr("class", "d-none");
+        $("#")
+    
+        
+    
+    }
 }
 
-renderQuestions();
+renderQuestions(questions);
+
